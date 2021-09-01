6c8e4788c4c1b6d6b29fed3c39e54ee89f2ca3e1
Ibrahima <ibracool99@gmail.com>
2021-09-01 18:38:50 +0100

    handleResponse in helpers, alert clear in auth pages

--- src/_helpers/index.js
+++ src/_helpers/index.js
@@ -1,4 +1,5 @@
 import {Redirect, Route} from "react-router-dom";
+import {userService} from "../services";
 
 export * from './fake-backend';
 export * from './history';
@@ -14,4 +15,24 @@ export function PrivateRoute ({component: Component, authed, ...rest}) {
         />
     )
 }
+export function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
 
+        if (!response.ok) {
+            if (response.status === 401) {
+                // auto logout if 401 response returned from api
+                userService.logout();
+                console.log(window.location.pathname);
+                if(!["/auth-cover-login","/auth-cover-signup"].includes(window.location.pathname)){
+                window.location.replace('auth-cover-login')
+                }
+            }
+
+            const error = (data && data.message) || response.statusText || (data && data.error);
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
+}

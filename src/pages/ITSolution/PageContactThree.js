09f96d47845dcf39174ae9e64a27eb09ac10782f
ambou-37 <ambroise.tine@gmail.com>
2021-09-09 20:40:12 +0100

    google map api

--- src/pages/ITSolution/PageContactThree.js
+++ src/pages/ITSolution/PageContactThree.js
@@ -315,7 +315,7 @@ class PageContactThree extends Component {
                                     <CardBody className="p-0">
                                         <iframe
                                             title="test"
-                                            src="//www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin"
+                                            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCWr_f0gQ-IDpw2Mh0P06IiuYo1E8ytOt0&q=cit%C3%A9%20de%20l'innovation%20de%20marrakech"
                                             style={{border: 0}}
                                             className="rounded"
                                             allowFullScreen=""
